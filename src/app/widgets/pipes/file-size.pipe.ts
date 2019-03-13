8cd1fc1de01af3d4c549ddbb3152d925852a776d
Jernej Strazisar <strazisar.jernej@gmail.com>
2019-03-13 10:56:21 +0100

    Added documentation for the file-size pipe.

--- src/app/widgets/pipes/file-size.pipe.ts
+++ src/app/widgets/pipes/file-size.pipe.ts
@@ -1,9 +1,26 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
+/**
+ * A pipe for converting the file size in bytes to the greatest possible unit
+ *  without hitting a decimal.
+ *
+ * @export
+ * @class FileSizePipe
+ * @implements {PipeTransform}
+ */
 @Pipe({
   name: 'fileSize'
 })
 export class FileSizePipe implements PipeTransform {
+  /**
+   *
+   *
+   * @param {number} originalSize - File size in bytes.
+   * @param {*} [args] - Not important.
+   * @returns {{ size: number; unit: string }} - Contains the size of the file
+   *  converted to a greater unit, along with the abbreviation.
+   * @memberof FileSizePipe
+   */
   transform(originalSize: number, args?: any): { size: number; unit: string } {
     const sizes = ['B', 'KB', 'MB', 'GB', 'TB'];
 
